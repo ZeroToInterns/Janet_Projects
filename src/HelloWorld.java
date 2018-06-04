40f7343414f52818e038edd3bc350da51acc2801
JANET ESCOBAR <janetescobar@JANETs-MacBook-Air.local>
2018-06-04 12:51:44 -0700

    Initial Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//This prints the word Hello World
+		System.out.println("Hello World");
+	}
+		
+}
